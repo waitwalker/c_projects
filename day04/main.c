24f61ecd67b81c99445237bc163dc4fd60ab2c91
waitwalker <waitwalker@163.com>
2021-03-26 09:45:31 +0800

    函数初步了解

---
+++ day04/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  day04
+//
+//  Created by waitwalker on 2021/3/26.
+//
+
+#include <stdio.h>
+/// 函数的声明
+/// 返回值类型 函数名（形参类型 形参）；
+
+
+void test(void);
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    test();
+    return 0;
+}
+
+/// 函数的实现
+void test() {
+    printf("This is test.\n");  
+}
+
