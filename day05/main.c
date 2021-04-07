733794d94c16bf631f456e099ea3abf451484a66
waitwalker <waitwalker@163.com>
2021-04-07 11:47:22 +0800

    指针项目初始化完成

---
+++ day05/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  day05
+//
+//  Created by waitwalker on 2021/4/7.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
