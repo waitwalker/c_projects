1829ee4a9fb53f0b4c267c633a25aca7810aefcc
waitwalker <waitwalker@163.com>
2021-04-08 09:39:52 +0800

    变量名代表的是空间的内容;&num代表的是变量num的起始地址(首地址)

--- day05/main.c
+++ day05/main.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    /// 内存是以字节为单位存储的
+    
     /// 32位系统中,任何指针(内存地址)都占4Byte
     /// 64位系统中,任何指针(内存地址)都占8字节
     
@@ -17,6 +19,13 @@ int main(int argc, const char * argv[]) {
     printf("long 类型指针占用空间:%lu\n",sizeof(long *));
     printf("double 类型指针占用空间:%lu\n",sizeof(double *));
     printf("float 类型指针占用空间:%lu\n",sizeof(float *));
+    
+    /// 定义num时,系统会为num分配一个合法的空间
+    int num = 10;
+    int *p;
+    p = &num;
+    
+    printf("%p\n",p);
     
     // insert code here...
     printf("Hello, World!\n");
