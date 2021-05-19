6835d80477004ff8dd3fa58fa3eebe066b4540f2
waitwalker <waitwalker@163.com>
2021-05-19 09:38:59 +0800

    辅助框架搭建

--- day10/main.c
+++ day10/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include "NodeClass.h"
 #include "link.h"
+#include <string.h>
 void stuHelp(void) {
     printf("#############################\n");
     printf("# help: 打印帮助信息          #\n");
@@ -23,7 +24,28 @@ void stuHelp(void) {
 int main(int argc, const char * argv[]) {
     // insert code here...
     //nodeOperation();
-    stuHelp();
+    while (1) {
+        char cmd[32] = "";
+        printf("请输入操作指令:");
+        scanf("%s",cmd);
+        if (strcmp(cmd, "help") == 0) {
+            printf("----- help 帮助 -----\n");
+            stuHelp();
+        } else if (strcmp(cmd, "insert") == 0) {
+            printf("----- insert 插入 -----\n");
+        } else if (strcmp(cmd, "print") == 0) {
+            printf("----- print 遍历 -----\n");
+        } else if (strcmp(cmd, "search") == 0) {
+            printf("----- search 查询 -----\n");
+        } else if (strcmp(cmd, "delete") == 0) {
+            printf("----- delete 删除 -----\n");
+        } else if (strcmp(cmd, "free") == 0) {
+            printf("----- free 释放 -----\n");
+        } else if (strcmp(cmd, "quit") == 0) {
+            printf("----- quit 退出 -----\n");
+            break;
+        }
+    }
     printf("Hello, World!\n");
     return 0;
 }
